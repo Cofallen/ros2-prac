8037c0fd427b4bdbd1609130bdb1b26af5b2468d
Cofallen <1727334304@qq.com>
2025-06-20 20:39:36 +0800

    🔹 auto: 2025-06-20 20:39:36

---
+++ cp3_topic/topic_practice_ws/build/status_interfaces/status_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cofallen/Code/ros2/cp3_topic/topic_practice_ws/build/status_interfaces/status_interfaces__py
+# Build directory: /home/cofallen/Code/ros2/cp3_topic/topic_practice_ws/build/status_interfaces/status_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
